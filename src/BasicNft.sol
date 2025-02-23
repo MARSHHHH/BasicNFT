85c1f3b7429b1bd9820bf2621ff53884b1fb992c
marsh <2636024301@qq.com>
2025-02-23 17:52:31 +0000

    Upload and use IPFS data

---
+++ src/BasicNft.sol
@@ -0,0 +1,27 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract BasicNft is ERC721 {
+    uint256 private s_tokenCounter;
+    mapping(uint256 => string) private s_tokenURIs;
+
+    constructor() ERC721("BasicNft", "BNFT") {
+        //Every time we update the new BNFT we will increment the token counter
+        s_tokenCounter = 0;
+    }
+
+    function mintNft(string memory tokenURI) public returns (uint256) {
+        s_tokenURIs[s_tokenCounter] = tokenURI;
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter++;
+    }
+
+    function tokenURI(
+        uint256 tokenId
+    ) public view override returns (string memory) {
+        return s_tokenURIs[tokenId];
+    }
+}
